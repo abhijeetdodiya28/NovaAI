af64997917cf08d264825e477347267f8e02c62c
abhijeetdodiya28 <abhijeetdodiya7@gmail.com>
2025-11-19 17:32:02 +0530

    this is my changes

--- Backend/middleware/googleAuth.js
+++ Backend/middleware/googleAuth.js
@@ -12,7 +12,7 @@ passport.use(
         {
             clientID: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            callbackURL: "http://localhost:7000/api/auth/google/callback",
+            callbackURL: "https://novaai-39kh.onrender.com/api/auth/google/callback",
         },
         async (accessToken, refreshToken, profile, done) => {
             try {
