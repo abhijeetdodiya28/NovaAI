6b597c7f25701a5139c05a138949464f58de8b1e
abhijeetdodiya28 <abhijeetdodiya7@gmail.com>
2025-11-20 22:17:36 +0530

    this is my changes

--- Backend/server.js
+++ Backend/server.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 7000;
 // ------------------- MIDDLEWARES -------------------
 const allowedOrigins = [
     "http://localhost:5173",
-    "https://nova-ai-red-six.vercel.app"
+    "https://nova-ai-sand-one.vercel.app"
 ];
 
 app.use(
